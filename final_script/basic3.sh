af2c59ac3c3c634d677913df7c6b0d1ad4f2e901
dinkar64 <dinkarrathi64@gmail.com>
2021-03-17 13:00:42 +0530

    file_contributors

--- final_script/basic3.sh
+++ final_script/basic3.sh
@@ -1,21 +1,21 @@
 cd food4all
-echo "	"  >> ../report.txt
-echo "File changes"  >> ../report.txt
-files=$(find $(pwd) -type f | awk '{printf "%s\n", $1}') 
+echo -e "/*Note : File contributors are mentioned in each file at eof */"  >> ../report.txt
+files=$(find $(pwd) -type f -name "*.js" | awk '{printf "%s\n", $1}') 
 IFS=$'\n'
 for file in $files
 do
-echo -e "$file" >> ../report.txt
+echo -e "/* File contributors */"  >> $file
+echo -e "/* $file */" >> $file
 users=$(git shortlog -sn --no-merges -- $file | awk '{printf "%s %s\n", $2, $3}')
 IFS=$'\n'
-echo -e "User name; Files changed; Lines added; Lines deleted; Commit count"  >> ../report.txt
+echo -e "/* User name; Lines added; Lines deleted; Commit count */"  >> $file
 for userName in $users
 do
-     result=$(git log --author="$userName" --no-merges --shortstat -- $file | grep -E "fil(e|es) changed" | awk '{files+=$1; inserted+=$4; deleted+=$6} END {printf "%s;%s;%s", files, inserted, deleted}' -)
+     result=$(git log --author="$userName" --no-merges --shortstat -- $file | grep -E "fil(e|es) changed" | awk '{inserted+=$4; deleted+=$6} END {printf "%s;%s", inserted, deleted}' -)
      countCommits=$(git shortlog -sn --no-merges --author="$userName" -- $file | awk '{print $1}')
      if [[ ${result} != ';;;;' ]]
      then
-        echo -e "$userName;$result;$countCommits"  >> ../report.txt
+        echo -e "/* $userName;$result;$countCommits */"  >> $file
      fi
 done
 done
